a23f0f49a975d5a9ebd453a59dda1ed2fe657757
Awennnnnn <78376598+Awennnnnn@users.noreply.github.com>
2022-11-11 13:15:26 +0100

    version0.000001

---
+++ src/component/Menu/Background.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import background from "../../assets/background/testreact.jpg";
+
+function Background() {
+    return <img src={background} alt="background"/>;
+}
+
+export default Background;
