a1e746821ef7d49d09e750996e8196cc88dae387
Alphée GROSDIDIER <alphee.grosdidier@gmail.com>
2022-12-16 20:17:19 +0100

    Adding a button to got directly to the dashboard
    Signed-off-by: Alphée GROSDIDIER <alphee.grosdidier@gmail.com>

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Container, Grid, Header } from 'semantic-ui-react'
+import { Button, Container, Grid, Header } from 'semantic-ui-react'
 import background from '../assets/background/testreact.jpg'
 import Card from '../component/ui/Card/card'
+import { Link } from 'react-router-dom'
 
 /*
 1. Enter the CyberArena and take your chances in the ultimate card game challenge!
@@ -15,55 +16,70 @@ import Card from '../component/ui/Card/card'
 
  */
 
+function FirstPartHomePage (): JSX.Element {
+    return (
+        <div style={{
+            background: `url(${background}) no-repeat center center fixed`,
+            backgroundSize: 'cover',
+            width: '100vw',
+            height: '100vh',
+            position: 'relative'
+        }}>
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle' columns={2} >
+                <Grid.Column
+                    style={{
+                        padding: '5px 10px 5px 10px',
+                        borderRadius: '10px',
+                        backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                    }}
+                    widescreen={6}
+                    largeScreen={6}
+                    computer={8}
+                    tablet={12}
+                    mobile={14}
+                >
+                    <Container textAlign='left'>
+                        <Header as='h1'>
+                            Welcome to CyberArena traveller!
+                        </Header>
+                        <p>
+                            Welcome to CyberArena, the ultimate card game experience!
+                            With stunning graphics and immersive gameplay, you&apos;ll feel like you&apos;ve stepped right into the cyber world. Test your skills and strategy against players from all over the world in this fast-paced, high-stakes card game. Challenge yourself to become a master of CyberArena and prove your worth on the virtual battlefield! Are you ready for an electrifying adventure? Play now and find out what CyberArena has to offer!
+                        </p>
+                        <Button
+                            primary={ true }
+                            content='Play Now'
+                            icon='play'
+                            labelPosition='right'
+                            floated={ 'right' }
+                            as={ Link }
+                            to={ '/dashboard' }
+                        />
+                    </Container>
+                </Grid.Column>
+                <Grid.Column only='large screen'>
+                    <Container textAlign='center'>
+                        <Card number={190}
+                            name={'First'}
+                            subtypes={'basic v'}
+                            supertype={'pokemon'}
+                            rarity={'rare ultra'}
+                            gallery={'false'}
+                            style={{ maxWidth: '35vmin', margin: 'auto' }}
+
+                            back_img={'https://images.pokemontcg.io/base1/1_hires.png'}
+                            front_img={'https://images.pokemontcg.io/base1/1_hires.png'}/>
+                    </Container>
+                </Grid.Column>
+            </Grid>
+        </div>
+    )
+}
+
 function HomePage (): JSX.Element {
     return (
         <>
-            <div style={{
-                background: `url(${background}) no-repeat center center fixed`,
-                backgroundSize: 'cover',
-                width: '100vw',
-                height: '100vh',
-                position: 'relative'
-            }}>
-                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle' columns={2} >
-                    <Grid.Column
-                        style={{
-                            padding: '5px 10px 5px 10px',
-                            borderRadius: '10px',
-                            backgroundColor: 'rgba(255, 255, 255, 0.5)'
-                        }}
-                        widescreen={6}
-                        largeScreen={6}
-                        computer={8}
-                        tablet={12}
-                        mobile={14}
-                    >
-                        <Container textAlign='left'>
-                            <Header as='h1'>
-                                Welcome to CyberArena traveller!
-                            </Header>
-                            <p>
-                                Welcome to CyberArena, the ultimate card game experience!
-                                With stunning graphics and immersive gameplay, you&apos;ll feel like you&apos;ve stepped right into the cyber world. Test your skills and strategy against players from all over the world in this fast-paced, high-stakes card game. Challenge yourself to become a master of CyberArena and prove your worth on the virtual battlefield! Are you ready for an electrifying adventure? Play now and find out what CyberArena has to offer!
-                            </p>
-                        </Container>
-                    </Grid.Column>
-                    <Grid.Column only='large screen'>
-                        <Container textAlign='center'>
-                            <Card number={190}
-                                name={'First'}
-                                subtypes={'basic v'}
-                                supertype={'pokemon'}
-                                rarity={'rare ultra'}
-                                gallery={'false'}
-                                style={{ maxWidth: '35vmin', margin: 'auto' }}
-
-                                back_img={'https://images.pokemontcg.io/base1/1_hires.png'}
-                                front_img={'https://images.pokemontcg.io/base1/1_hires.png'}/>
-                        </Container>
-                    </Grid.Column>
-                </Grid>
-            </div>
+            <FirstPartHomePage/>
         </>
     )
 }
