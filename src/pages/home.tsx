5bd74f87377b21f9936213184e9776a138e1c611
Alphée GROSDIDIER <alphee.grosdidier@gmail.com>
2022-12-16 01:04:30 +0100

    Create the first part of homepage
    Signed-off-by: Alphée GROSDIDIER <alphee.grosdidier@gmail.com>

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,60 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import {Container, Grid, Header} from 'semantic-ui-react'
 import background from '../assets/background/testreact.jpg'
+
+/*
+1. Enter the CyberArena and take your chances in the ultimate card game challenge!
+2. Face off against opponents from around the world and prove you're the best in this epic cyber showdown!
+3. Step into a digital world where every move counts - can you become king of CyberArena?
+4. Unleash your strategic genius and show them who's boss in an electrifying game of cards!
+5. Get ready for an adventure like no other - welcome to CyberArena!
+ */
 
 function HomePage (): JSX.Element {
     return (
-        <div style={{ backgroundImage: `url(${background})`, width: '100vw', height: '100vh' }}>
-            <Container fluid={true} style={{ marginTop: '7em' }}>
-                <h1>Home Page</h1>
-            </Container>
-        </div>
-
+        <>
+            <div style={{
+                background: `url(${background}) no-repeat center center fixed`,
+                backgroundSize: 'cover',
+                width: '100vw',
+                height: '100vh',
+                position: 'relative'
+            }}>
+                <Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle' columns={2} >
+                    <Grid.Column
+                        style={{
+                            padding: '5px 10px 5px 10px',
+                            borderRadius: '10px',
+                            backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                        }}
+                        widescreen={6}
+                        largeScreen={6}
+                        computer={8}
+                        tablet={12}
+                        mobile={14}
+                    >
+                        <Container textAlign='left'>
+                            <Header as='h1'>
+                                Welcome to CyberArena traveller!
+                            </Header>
+                            <p>
+                                Welcome to CyberArena, the ultimate card game experience! With stunning graphics and immersive gameplay, you'll feel like you've stepped right into the cyber world. Test your skills and strategy against players from all over the world in this fast-paced, high-stakes card game. Challenge yourself to become a master of CyberArena and prove your worth on the virtual battlefield! Are you ready for an electrifying adventure? Play now and find out what CyberArena has to offer!
+                            </p>
+                        </Container>
+                    </Grid.Column>
+                    <Grid.Column only='large screen'>
+                        <Header as='h2' color='teal' textAlign='center'>
+                            Welcome to React
+                        </Header>
+                        <Container>
+                            <p>
+                                Enter the thrilling world of CyberArena, where strategy and skill determine your victory. Challenge yourself against players from around the world and see if you have what it takes to be crowned champion of the virtual arena! Join now and test your mettle in a digital duel that will push you to your limits.
+                            </p>
+                        </Container>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        </>
     )
 }
 
