fc9b8800c0ebb2c8b859f71c911a347f4da13a51
Awennnnnn <78376598+Awennnnnn@users.noreply.github.com>
2023-01-29 20:08:13 +0100

    Homepage animation & text

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Button, Container, Divider, Embed, Grid, Header, Image, Segment } from 'semantic-ui-react'
 import Card from '../component/ui/Card/card'
 import { Link } from 'react-router-dom'
+import {WebsiteOverlay} from "../component/ui/homeElements/WebsiteOverlay";
+import {VideoBackground} from "../component/ui/homeElements/VideoBackground";
 
 const background = '/img/background/testreact.jpg'
 const background2 = '/img/background/testreact2.jpg'
@@ -20,64 +22,9 @@ const background2 = '/img/background/testreact2.jpg'
 
 function FirstPartHomePage (): JSX.Element {
     return (
-        <div style={{
-            background: `url(${background}) no-repeat center center fixed`,
-            backgroundSize: 'cover',
-            width: '100%',
-            height: '100vh',
-            position: 'relative'
-        }}>
-            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle' columns={2}>
-                <Grid.Column
-                    style={{
-                        padding: '5px 10px 5px 10px',
-                        borderRadius: '10px',
-                        backgroundColor: 'rgba(255, 255, 255, 0.5)'
-                    }}
-                    widescreen={6}
-                    largeScreen={6}
-                    computer={8}
-                    tablet={12}
-                    mobile={14}
-                >
-                    <Container textAlign='left'>
-                        <Header as='h1'>
-                            Welcome to CyberArena, traveller!
-                        </Header>
-                        <p>
-                            Welcome to CyberArena, the ultimate card game experience!
-                            With stunning graphics and immersive gameplay, you&apos;ll feel like you&apos;ve stepped
-                            right into the cyber world. Test your skills and strategy against players from all over the
-                            world in this fast-paced, high-stakes card game. Challenge yourself to become a master of
-                            CyberArena and prove your worth on the virtual battlefield! Are you ready for an
-                            electrifying adventure? Play now and find out what CyberArena has to offer!
-                        </p>
-                        <Button
-                            primary={true}
-                            content='Play Now'
-                            icon='play'
-                            labelPosition='right'
-                            floated={'right'}
-                            as={Link}
-                            to={'/dashboard'}
-                        />
-                    </Container>
-                </Grid.Column>
-                <Grid.Column only='large screen'>
-                    <Container textAlign='center'>
-                        <Card number={190}
-                            name={'First'}
-                            subtypes={'basic v'}
-                            supertype={'pokemon'}
-                            rarity={'rare ultra'}
-                            gallery={'false'}
-                            style={{ maxWidth: '35vmin', margin: 'auto' }}
-
-                            back_img={'https://images.pokemontcg.io/base1/1_hires.png'}
-                            front_img={'https://images.pokemontcg.io/base1/1_hires.png'}/>
-                    </Container>
-                </Grid.Column>
-            </Grid>
+        <div className="flex relative w-full h-full">
+            <WebsiteOverlay/>
+            <VideoBackground/>
         </div>
     )
 }
@@ -185,27 +132,20 @@ function ThirdPartHomePage (): JSX.Element {
                     tablet={12}
                     mobile={14}
                 >
-                    <Container textAlign='left'>
-                        <Header as='h2'>
-                            Discover the world of CyberArena!
-                        </Header>
-                        <p>
-                            CyberArena take place in a futuristic world where humans and machines live side by side. The
-                            world is
-                            divided into four factions, each with their own unique play style. Choose your faction and
-                            fight for
-                            glory in the cyber arena!
-                        </p>
-                        <Button
-                            primary={true}
-                            content='Play Now'
-                            icon='play'
-                            labelPosition='right'
-                            floated={'right'}
-                            as={Link}
-                            to={'/dashboard'}
-                        />
-                    </Container>
+                    <div className="flex absolute w-full h-full z-10 p-10 text-white">
+                        <div className="flex font-bold flex-col my-auto max-w-[38rem] backdrop-blur-[40px] p-4 rounded-3xl">
+                            <div className="uppercase text-8xl">Discover</div>
+                            <div className="uppercase text-4xl mb-6">The CyberWorld</div>
+                            <div className="mb-6">
+                                CyberArena take place in a futuristic world where humans and machines live side by side. The
+                                world is
+                                divided into four factions, each with their own unique play style. Choose your faction and
+                                fight for
+                                glory in the cyber arena!
+                            </div>
+                        </div>
+                    </div>
+
                 </Grid.Column>
             </Grid>
         </div>
