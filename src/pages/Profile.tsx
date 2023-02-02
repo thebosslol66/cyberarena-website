b95f025da548f07ecfc76be186f00a224d1f17a3
Awennnnnn <78376598+Awennnnnn@users.noreply.github.com>
2023-02-02 15:29:30 +0100

    profile

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Grid, Segment } from 'semantic-ui-react'
+
+function ProfilePage (): JSX.Element {
+    return (
+        <Segment basic style={{ paddingTop: '6em' }} fluid="true">
+
+        </Segment>
+    )
+}
+
+export default ProfilePage
