ac5bd253ede77c4e1059e121e6be86ebaa743262
Alphée GROSDIDIER <alphee.grosdidier@gmail.com>
2023-02-02 13:28:42 +0100

    Adding my implem of drag and drop
    Signed-off-by: Alphée GROSDIDIER <alphee.grosdidier@gmail.com>

---
+++ src/component/Game/DropZone.tsx
@@ -0,0 +1,34 @@
+import { CardModel } from '../../client'
+import React from 'react'
+import { Droppable } from 'react-beautiful-dnd'
+import { DraggableCard } from './DraggableCard'
+
+interface Props {
+    isDropDisabled: boolean
+    cards: CardModel[]
+    id: string
+    color: string
+    height: string
+    width: string
+}
+
+export class DropZone extends React.Component<Props, {}> {
+    render (): JSX.Element {
+        return (
+            <Droppable droppableId={this.props.id} direction="vertical" isDropDisabled={this.props.isDropDisabled}>
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        style={{ backgroundColor: this.props.color, width: this.props.width, height: this.props.height, margin: 'auto' }}
+                    >
+                        {this.props.cards.map((card, index) => (
+                            <DraggableCard index={index} card={card} key={card.id}/>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        )
+    }
+}
--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -4,67 +4,34 @@
 import { Button } from 'semantic-ui-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { DragDropContext } from 'react-beautiful-dnd'
-import Dropzone from '../component/ui/D&D/Dropzone'
-import { GAME_STATE, move } from '../component/ui/D&D/utils'
-import { GameService } from '../client/services/GameService'
 import { CardModel } from '../client'
-import {WebsiteOverlay} from "../component/ui/homeElements/WebsiteOverlay";
-import {VideoBackground} from "../component/ui/homeElements/VideoBackground";
+import {Board} from "../component/Game/Board";
+import {GAME_STATE} from "../component/ui/D&D/utils";
+
+let board = {
+
+    cards_on_board: {
+        1: {
+            id: 1,
+            name: 'Heisenberg',
+            description: 'useless',
+            cost: 10,
+            damage: 5,
+            health: 22,
+            defense: 13
+        } as CardModel,
+        2: {id: 2, name: 'Walter', description: 'useless', cost: 10, damage: 5, health: 22, defense: 13} as CardModel,
+        3: {id: 3, name: 'Jesse', description: 'useless', cost: 10, damage: 5, health: 22, defense: 13} as CardModel,
+        4: {id: 4, name: 'Saul', description: 'useless', cost: 10, damage: 5, health: 22, defense: 13} as CardModel,
+
+    },
+    main_1: [1, 2, 3, 4],
+}
 
 const background = '/img/background/arena1.png'
 
-interface State {
-    gameState: typeof GAME_STATE.READY
-    main_1: CardModel[]
-    main_2: CardModel[]
-    plateau_1: CardModel[]
-    plateau_2: CardModel[]
-}
-export default class GamePage extends React.Component < {}, State > {
-    componentDidMount () {
-        this.getCardInfo(1)
-    }
-    constructor (props: any) {
-        super(props)
-        this.state = {
-            gameState: GAME_STATE.READY,
-            main_1: [],
-            main_2: [],
-            plateau_1: [],
-            plateau_2: []
-        }
-    }
 
-    getCardInfo = (count: number) => {
-        GameService.getCardApiGameCardCardIdDataGet(1).then((response) => {
-            this.setState({ main_1: this.state.main_1.concat(response) })
-        })
-    }
-
-    startGame = () => {
-        this.setState(
-            {
-                gameState: GAME_STATE.PLAYING
-            }
-        )
-    }
-
-    onDragEnd = (source: any, destination: any) => {
-        if (!destination) {
-            return
-        }
-        this.setState(state => {
-            return move(state, source, destination)
-        })
-    }
-
-    endGame = () => {
-        this.setState({
-            gameState: GAME_STATE.DONE
-        })
-    }
-
+export default class GamePage extends React.Component <{ }, { } > {
     render () {
         return (
             <div style={{
@@ -75,12 +42,7 @@ export default class GamePage extends React.Component < {}, State > {
                 position: 'relative'
             }}>
                 <Button icon='remove' content='Leave Game' as={Link} to='/dashboard' negative={ true } floated={ 'right' } style={{ marginTop: '2em', marginRight: '1em' }}/>
-                <DragDropContext onDragEnd={ this.onDragEnd }>
-                    <Dropzone id="deck1" cards={ this.state.main_1 } isDropDisabled={ false } color={ 'red' } height={ '15%' } width={ '50%' }/>
-                    <Dropzone id="plateau1" cards={ this.state.plateau_1 } isDropDisabled={ false } color={ 'blue' } height={ '30%' } width={ '100%' }/>
-                    <Dropzone id="plateau2" cards={ this.state.plateau_2 } isDropDisabled={ false } color={ 'green' } height={ '30%' } width={ '100%' }/>
-                    <Dropzone id="deck2" cards={ this.state.main_2 } isDropDisabled={ false } color={ 'yellow' } height={ '25%' } width={ '70%' }/>
-                </DragDropContext>
+                <Board  board={board}></Board>
             </div>
         )
     }
