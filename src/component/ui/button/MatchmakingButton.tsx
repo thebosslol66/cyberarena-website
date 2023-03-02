12518ac0f31d5d54a69fffb2d0600382647044bc
Awennnnnn <78376598+Awennnnnn@users.noreply.github.com>
2023-03-03 00:29:02 +0100

    Game is found when 2 players are searching

--- src/component/ui/button/MatchmakingButton.tsx
+++ src/component/ui/button/MatchmakingButton.tsx
@@ -1,61 +1,47 @@
-/* tslint:disable */
-/* eslint-disable */
 import { Button, Header, Icon, Loader, Modal } from 'semantic-ui-react'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import GameService from '../../../services/game.service'
-import {TicketData} from "../../../services/Interfaces/game";
+import { TicketData } from '../../../services/Interfaces/game'
 
 export default class MatchmakingButton extends React.Component {
-    state = { ticketID: '-1', open: false, room_id: ''}
-    private timeout: NodeJS.Timeout | undefined;
-    componentDidUpdate(prevProps: any) {
-        // Vérifiez si l'attribut ticketID a été mis à jour
-        if (this.state.ticketID !== prevProps.ticketID) {
-            // Effectuez les opérations nécessaires ici
-            console.log("ticketID updated: ", this.state.ticketID);
+    state = { ticketID: '-1', open: false, room_id: null, playable: false }
+    private timeout: NodeJS.Timeout | undefined
+    private readonly interval: number = 1000
 
-        }
-    }
-
-    quitMachmaking = () => {
+    quitMachmaking = (): void => {
         GameService.cancel_ticket(this.state.ticketID).then(() => {
-            this.setState({ ticketID : '-1', open: false })
-            clearTimeout(this.timeout as NodeJS.Timeout);
+            this.setState({ ticketID: '-1', open: false })
+            clearTimeout(this.timeout as NodeJS.Timeout)
         }).catch((error) => {
             console.log(error)
         })
     }
 
-    OpenMatchmaking = () => {
-        GameService.open_ticket().then((response : TicketData) => {
-            console.log("resp id :"+response.id)
-            console.log("id :"+this.state.ticketID)
-            this.setState({ ticketID : response.id })
+    OpenMatchmaking = (): void => {
+        GameService.open_ticket().then((response: TicketData) => {
+            this.setState({ ticketID: response.id })
             this.timeout = setTimeout(() => {
-                    this.fetchTicketStatus(this.state.ticketID);
-            })
+                this.fetchTicketStatus()
+            }, this.interval)
         }).catch((error) => {
             console.log(error)
         })
     }
 
-    fetchTicketStatus = (ticketID: string) => {
-        GameService.ticket_status(ticketID).then((response: TicketData) => {
-                if (response.room_id != null) {
-                    // Si le ticket a été associé à une salle, mettez à jour le state room_id
-                    this.setState({room_id: response.room_id});
-                }
-                if (this.state.ticketID == '-1') {
-                    this.timeout = setTimeout(() => {
-                        this.fetchTicketStatus(this.state.ticketID);
-                    })
-                }
-            }).catch((error) => {
-                console.log(error);
-            });
+    fetchTicketStatus = (): void => {
+        GameService.ticket_status(this.state.ticketID).then(response => {
+            if (response.room_id !== -1) {
+                this.setState({ room_id: response.room_id, playable: true })
+            } else {
+                setTimeout(this.fetchTicketStatus, this.interval)
+            }
+        }).catch(error => {
+            console.log(error)
+        })
     }
-    render () {
+
+    render (): JSX.Element {
         return (
             <Modal
                 basic
@@ -64,15 +50,26 @@ export default class MatchmakingButton extends React.Component {
                 size='small'
                 trigger={<Button primary size='massive' style={ { marginBottom: '1em' } } onClick={this.OpenMatchmaking}>Search for a player</Button>}
             >
-                <Loader active/>
-                <Header as='h2' textAlign='center' style={ { paddingBottom: '3em' }}>
-                    Searching for a player...
-                </Header>
+                {this.state.playable
+                    ? (
+                        <Header as='h2' textAlign='center' style={ { paddingBottom: '3em' }}>
+                            Game found !
+                        </Header>
+                    )
+                    : (
+                        <>
+                            <Loader active/>
+                            <Header as='h2' textAlign='center' style={ { paddingBottom: '3em' }}>
+                                Searching for a player...
+                            </Header>
+                        </>
+
+                    )}
                 <Modal.Actions>
-                    <Button basic color='red' inverted onClick={this.quitMachmaking}>
+                    <Button disabled={this.state.playable} basic color='red' inverted onClick={this.quitMachmaking}>
                         <Icon name='remove' /> Cancel
                     </Button>
-                    <Button basic color='blue' inverted as={NavLink} to={'/game'}>
+                    <Button disabled={!this.state.playable} basic color='blue' inverted as={NavLink} to={'/game'}>
                         <Icon name='check circle' /> Start game
                     </Button>
                 </Modal.Actions>
