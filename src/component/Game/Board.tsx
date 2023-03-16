5da8d17ad655f9e89a12eaafebf7fbff86a2738e
FT1Gauthier <fantin.gauthier@gmail.com>
2023-03-16 18:53:09 +0100

    affichage mana dynamique affichage carte statique plateau opps

--- src/component/Game/Board.tsx
+++ src/component/Game/Board.tsx
@@ -4,6 +4,7 @@ import { GAME_STATE } from '../ui/D&D/utils'
 import { CardModel } from '../../client'
 import { DropZone } from './DropZone'
 import {Deck} from "./Deck";
+import {PlateauOpps} from "./plateauOpps";
 
 export interface BoardData {
     main_1: number[]
@@ -55,7 +56,6 @@ export class Board extends React.Component<BoardProps , BoardState> {
                     },
                 }
             )
-            console.log('componentDidUpdate')
         }
 
     }
@@ -100,7 +100,7 @@ export class Board extends React.Component<BoardProps , BoardState> {
         return (
             <>
                 <Deck cards={ this.state.board.main_2.map(cardId => this.state.board.cards_on_board[cardId]) } color={ 'red' } height={ '15%' } width={ '50%' }/>
-                <Deck cards={ this.state.board.plateau_2.map(cardId => this.state.board.cards_on_board[cardId]) } color={ 'blue' } height={ '30%' } width={ '100%' }/>
+                <PlateauOpps cards={ this.state.board.plateau_2.map(cardId => this.state.board.cards_on_board[cardId]) } color={ 'blue' } height={ '30%' } width={ '100%' }/>
                 <DragDropContext onDragEnd={this.onDragEnd}>
                     <DropZone id="plateau_1" cards={ this.state.board.plateau_1.map(cardId => this.state.board.cards_on_board[cardId]) } isDropDisabled={this.props.dropDisabled} color={ 'green' } height={ '30%' } width={ '100%' }/>
                     <DropZone id="main_1" cards={ this.state.board.main_1.map(cardId => this.state.board.cards_on_board[cardId]) } isDropDisabled={this.props.dropDisabled} color={ 'yellow' } height={ '25%' } width={ '70%' }/>
